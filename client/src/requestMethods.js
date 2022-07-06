cee62f150507a6aa7d65c5086c3378c1c4f0f481
Trung Nguyên <nguyennguyentrung01@gmail.com>
2022-07-07 00:30:32 +0700

    create page admin

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const TOKEN =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYzRmNWU0MGZhZWNlMzQyYzdmOTVmMyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NzA5NTA3NywiZXhwIjoxNjU3MzU0Mjc3fQ.pm-gtCf1cVdOX_viynWvmJR9a2FqP_DShdMANHGxR7M"
+const TOKEN =  JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 const BASE_URL = "http://localhost:5000/api/";
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
